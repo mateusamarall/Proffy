f957776ee2bf730db9c588f1122d2597c7e13901
mateusamarall <mateus.amaral018@gmail.com>
2020-08-04 08:40:22 -0300

    Importado Link para navegação de páginas

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LogoImg from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
 import StudyIcon from "../../assets/images/icons/study.svg";
@@ -20,14 +21,14 @@ function Landing() {
           className="hero-image"
         />
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={StudyIcon} alt="Estudar" />
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={GiveClassesIcon} alt="Dar aulas" />
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas{" "}
